ed8b83a3d4b004d48b55ad81fb4f4901d7c0da00
ryoutoku <invisible060@gmail.com>
2022-08-03 07:33:29 +0900

    feat: lint settings for lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,29 @@
 module.exports = {
   root: true,
+
   env: {
     node: true,
   },
+
   extends: [
     'plugin:vue/vue3-essential',
     '@vue/airbnb',
     '@vue/typescript/recommended',
+    'prettier',
   ],
+
   parserOptions: {
     ecmaVersion: 2020,
+    ecmaFeatures: {
+      jsx: true,
+    },
   },
+
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
   },
+
   overrides: [
     {
       files: [
